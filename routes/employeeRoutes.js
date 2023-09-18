a17961cb32080cf8291fe784b3b94e3da32a14ac
IamIronMan2023 <kramsotnas@yahoo.com>
2023-09-18 20:10:22 +0800

    remove employee route auth middleware

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -10,10 +10,10 @@ import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/", auth, getAllEmployees);
-router.get("/:id", auth, getEmployee);
-router.post("/", auth, createEmployee);
-router.patch("/:id", auth, updateEmployee);
-router.delete("/:id", auth, deleteEmployee);
+router.get("/", getAllEmployees);
+router.get("/:id", getEmployee);
+router.post("/", createEmployee);
+router.patch("/:id", updateEmployee);
+router.delete("/:id", deleteEmployee);
 
 export default router;
